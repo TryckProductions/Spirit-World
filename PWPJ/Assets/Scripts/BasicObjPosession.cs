c835a1b81ae79ad9e95f69dbad59a81716d3ae61
Taylor Zieman <theeburntgamer@gmail.com>
2018-03-16 21:09:42 -0500

    Move
    stuff

---
+++ PWPJ/Assets/Scripts/BasicObjPosession.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicObjPosession : MonoBehaviour {
+
+    public bool isPos;
+
+    GameObject objPosessed;
+    public Transform lastPos;
+
+	// Use this for initialization
+	void Start()
+    {
+        isPos = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1) && isPos == true)
+        {
+
+            gameObject.transform.position = lastPos.position;
+            isPos = false;
+
+        }
+    }
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.tag == "posObj")
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                if (isPos == false)
+                {
+                    objPosessed = other.gameObject;
+                    basicPos();
+                }
+                /*
+                else if(isPos == true)
+                {
+                    gameObject.transform.position = lastPos.position;
+                    isPos = false;
+                }
+                */   
+            }
+        }
+    }
+
+    private void basicPos()
+    {
+        lastPos = gameObject.transform;
+        //animation
+        objPosessed.GetComponent<BoxCollider2D>().isTrigger = true;
+        gameObject.transform.position = objPosessed.transform.position;
+        isPos = true;
+    }
+
+}
--- PWPJ/Assets/Scripts/PlayerMovement.cs
+++ PWPJ/Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,50 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
+    public float MoveSpd; //Declaring the Move Speed  --Make Private--
+    private float HorMulti = 1.3f;  //Increasing the Move Speed When Walking Horizontally
+
+    Rigidbody2D pRgd; //Declaring The Player's Rigidbody Variable
 	// Use this for initialization
 	void Start () {
-		
+        pRgd = gameObject.GetComponent<Rigidbody2D>();  //Attatching the Player's Rigidbody Variable to an ingame Rigidbody
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    private void FixedUpdate()
+    {
+        pMove();  //Runs the pMove() Function
+    }
+
+    private void pMove()
+    {
+        if (Input.GetKey(KeyCode.W))  //If Pressing W, Move Up
+        {
+
+            pRgd.velocity = new Vector2(pRgd.velocity.x, MoveSpd * 1);
+
+        }
+        else if (Input.GetKey(KeyCode.S))  //If Pressing S, Move Down
+        {
+            pRgd.velocity = new Vector2(pRgd.velocity.x, -MoveSpd * 1);
+        }
+
+        else pRgd.velocity = new Vector2(pRgd.velocity.x, 0);
+
+
+        if (Input.GetKey(KeyCode.D))  //If Pressing D, Move Right
+        {
+            pRgd.velocity = new Vector2((MoveSpd * HorMulti) * 1, pRgd.velocity.y);
+        }
+        else if (Input.GetKey(KeyCode.A))  //If Pressing A, Move Left
+        {
+            pRgd.velocity = new Vector2((-MoveSpd * HorMulti) * 1, pRgd.velocity.y);
+        }
+
+        else pRgd.velocity = new Vector2(0, pRgd.velocity.y);
+    }
 }
